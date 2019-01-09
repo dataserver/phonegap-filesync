6a9e9aaa8397a2574d0b019de033240136ddae3b
dataserver <dataserver@users.noreply.github.com>
2019-01-09 16:13:03 -0200

    upload initial

---
+++ www/js/main.js
@@ -0,0 +1,193 @@
+((() => { // avoid conflicts
+    const DEFAULT_CONFIG = {
+        url : ''
+    };
+
+    // https://stackoverflow.com/questions/7298364/using-jquery-and-json-to-populate-forms
+    function populateForm($form, data) {
+        $.each(data, (key, value) => {// all json fields ordered by name
+            let $ctrls, $ctrl;
+
+            if (value instanceof Array){
+                $ctrls = $form.find("[name='" + key + "[]']"); //all form elements for a name. Multiple checkboxes can have the same name, but different values
+            } else {
+                $ctrls = $form.find("[name='" + key + "']");
+            }
+            if ($ctrls.is("select")) { //special form types
+                $("option", $ctrls).each(function() {
+                    if (this.value == value) {
+                        this.selected = true;
+                    }
+                });
+            } else if ($ctrls.is("textarea")) {
+                $ctrls.val(value);
+            } else {
+                switch ($ctrls.attr("type")) { //input type
+                    case "text":
+                    case "range":
+                    case "hidden":
+                        $ctrls.val(value);
+                        break;
+                    case "radio":
+                        if ($ctrls.length >= 1) {
+                            $.each($ctrls, function(index) { // every individual element
+                                let elemValue = $(this).attr("value");
+                                let singleVal = value;
+                                let elemValueInData = singleVal;
+
+                                if (elemValue == value) {
+                                    $(this).prop("checked", true);
+                                } else {
+                                    $(this).prop("checked", false);
+                                }
+                            });
+                        }
+                        break;
+                    case "checkbox":
+                        if ($ctrls.length > 1) {
+                            $.each($ctrls, function(index) { // every individual element
+                                let elemValue = $(this).attr("value");
+                                let elemValueInData;
+                                let singleVal;
+
+                                for (let i=0; i<value.length; i++) {
+                                    singleVal = value[i];
+                                    debug("singleVal", singleVal, "/value[i][1]", value[i][1]);
+                                    if (singleVal == elemValue) {
+                                        elemValueInData = singleVal;
+                                    }
+                                }
+                                if (elemValueInData) {
+                                    $(this).prop("checked", true);
+                                } else {
+                                    $(this).prop("checked", false);
+                                }
+                            });
+                        } else if ($ctrls.length == 1) {
+                            $ctrl = $ctrls;
+                            if (value) {
+                                $ctrl.prop("checked", true);
+                            } else {
+                                $ctrl.prop("checked", false);
+                            }
+                        }
+                        break;
+                }
+            }
+        });
+    }
+
+    function nl2br (str) {
+        str = (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br>$2');
+        str = str.replace(/  /g, '&nbsp; ');
+        return str;
+    }
+
+    // CONFIG
+    function get_config() {
+        // let raw = localStorage.getItem('configs');
+        // let cfg = JSON.parse(raw);
+        let cfg = storage.get('configs');
+
+        return Object.assign({}, DEFAULT_CONFIG, cfg);
+    }
+    function set_config(cfg) {
+
+        //localStorage.setItem("configs", JSON.stringify(cfg));
+        storage.set('configs', cfg);
+        return cfg;
+    }
+    // Synced file
+    function get_syncfile() {
+        //let raw = localStorage.getItem('texto');
+        //return JSON.parse(raw);
+        return storage.get('texto');
+    }
+    function set_syncfile(content="") {
+
+        //localStorage.setItem("texto", JSON.stringify(content));
+        storage.set('texto', content);
+
+        return content;
+    }
+
+    function updateText() {
+        let configs = get_config();
+        let textSaved = get_syncfile();
+
+        if (configs.url=="") {
+            $("#on-off-status").html("URL to file not defined");
+            $("#texto").html("Configure the URL to sync");
+        } else {
+            $.ajax({
+                url : configs.url,
+                cache: false,
+            }).done(function(result){
+                let newText = nl2br(result);
+
+                $("#on-off-status").html("online");
+                $("#texto").html(newText);
+                set_syncfile(newText);
+            }).fail(function() {
+                $("#on-off-status").html("online failed");
+                $("#texto").html(textSaved);
+            });
+        }
+    }
+
+    $(document).ready(function() {
+        let connectionStatus = "offline";
+        let textSaved = get_syncfile();
+        let configs = get_config();
+
+        connectionStatus = navigator.onLine ? "online" : "offline";
+
+        if (connectionStatus == "offline") {
+            $("#texto").html(textSaved);
+        } else {
+           updateText();
+        }
+
+        $(document).on("pagecontainershow", function () {
+            var activePage = $.mobile.pageContainer.pagecontainer("getActivePage");
+
+            var activePageId = activePage[0].id;
+            switch (activePageId) {
+                case 'page-config':
+                    populateForm($("#form-configs"), configs);
+                    break;
+                case 'page-main':
+                    break;
+                case 'page-x':
+                    break;
+                default:
+            }
+        });
+
+        $("#js-sync-get").click(function(){
+            event.preventDefault();
+            if (configs.url=="") {
+                $("#on-off-status").html("URL to file not defined");
+                $("#texto").html("Configure the URL to sync");
+            } else {
+                updateText();
+            }
+        });
+        $("#js-configs-save").click(function(){
+            event.preventDefault();
+            let formData = $("#form-configs").serializeJSON();
+
+            configs = set_config(formData);
+
+            $(':mobile-pagecontainer').pagecontainer('change', '#page-main', {
+                transition: 'flip',
+                changeHash: false,
+                reverse: true,
+                showLoadMsg: true
+            });
+            updateText();
+        });
+    });
+
+
+}))();
---
+++ www/js/storage.js
@@ -0,0 +1,111 @@
+let storage = {
+    version : 1,
+    enabled : true,
+    compress: true,
+    options : {
+        prefix : ''
+    },
+
+    get : function(key, defaultValue = '') {
+        return this._read(key, defaultValue);
+    },
+    set : function(key, value) {
+        this._write(key, value);
+    },
+    remove : function(key) {
+        this._delete(key);
+    },
+    empty : function() {
+        this._clear();
+    },
+    getAll : function() {
+        const keys = this._keys();
+        let obj = {};
+
+        for (let i=0, len=keys.length; i<len; i++) {
+            obj[keys[i]] = this._read(keys[i]);
+        }
+        return obj;
+    },
+    getKeys : function() {
+        return this._keys();
+    },
+    getPrefix : function() {
+        return this.options.prefix;
+    },
+    has : function(key) {
+        return this.get(key, null) !== null;
+    },
+    forEach : function(callbackFunc) {
+        const allContent = this.getAll();
+
+        for (let prop in allContent) {
+            callbackFunc(prop, allContent[prop]);
+        }
+    },
+
+
+    // localStorage API
+    _read : function(key, defaultValue) {
+        let raw = localStorage.getItem(this._prefix(key));
+        
+        if (!raw) {
+            return defaultValue;
+        }
+        // let str = (this.compress) ? LZString.decompressFromUTF16(raw) : raw;
+        return this._unserialize(raw);
+    },
+    _write : function(key, value) {
+        let serialized = this._serialize(value);
+        // let str = (this.compress) ? LZString.compressToUTF16(serialized) : serialized;
+        return localStorage.setItem(this._prefix(key), serialized);
+    },
+    _delete : function(key) {
+        return localStorage.removeItem(this._prefix(key));
+    },
+    _keys : function() {
+        let unprefixed = [];
+
+        for (let i=0, len=localStorage.length; i<len; ++i ) {
+            unprefixed[i] =  this._unprefix(mystorage.key(i));
+        }
+        return unprefixed;
+    },
+    _clear : function() {
+        localStorage.clear();
+    },
+    // localStorage API
+
+    _prefix : function(key) {
+        return this.options.prefix + key;
+    },
+    _unprefix : function(key) {
+        return key.replace(new RegExp("^" + this.options.prefix), '');
+    },
+    _serialize : function(value) {
+        if (this._isJson(value)) {
+            return value;
+        }
+        return JSON.stringify(value);
+    },
+    _unserialize : function(value) {
+        if (this._isJson(value)) {
+            return JSON.parse(value);
+        }
+        return value;
+    },
+    _isJson : function(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    },
+    _isObject : function(a) {
+        return (!!a) && (a.constructor === Object);
+    },
+    _isArray : function(a) {
+        return (!!a) && (a.constructor === Array);
+    }
+};
